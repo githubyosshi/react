9c28fc17a250eb47f383b9814b64c5feabccabb1
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-23 22:28:28 +0900

    Jsx commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return(
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default App;
+
+render() {
+  return (
+      <div>
+          <h1>h1です</h1>
+          <h2>h2です</h2>
+          <p>pです</p>
+      </div>
+  );
+}
