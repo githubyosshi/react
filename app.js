a4177732a84725efb4e883e3bb6dc109a703e77a
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-24 12:05:40 +0900

    State commit

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name:'にんじゃわんこ'};
+  }
+  handleClick(name){
+    this.setState({name:name});
+  }
+
   render(){
+    console.log(this.state);
     return(
-      <h1>Hello World</h1>
+      <div>
+        <h1>こんにちは、{this.state.name}さん！</h1>
+        {/* <button onClick={() => {console.log('ひつじ仙人')}}>ひつじ仙人</button> */}
+        {/* <button onClick={() => {this.setState({name:'ひつじ仙人'})}}>ひつじ仙人</button> */}
+        <button onClick={() => {this.handleClick('ひつじ仙人')}}>ひつじ仙人</button>
+        <button onClick={() => {console.log('忍者わんこ')}}>忍者わんこ</button>
+      </div>
     );
   }
 }
-
 export default App;
-
-render() {
-  return (
-      <div>
-          <h1>h1です</h1>
-          <h2>h2です</h2>
-          <p>pです</p>
-      </div>
-  );
-}
